316f2546fc17e1dff290a4d8ebd15f9470a20c9c
Nate Rowan <nrowan@Nates-MacBook-Pro.local>
2013-02-22 20:58:35 -0800

    Initial Commit

---
+++ FallingBlocks/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  FallingBlocks
+//
+//  Created by Nate Rowan on 2/22/13.
+//  Copyright (c) 2013 Nate Rowan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
